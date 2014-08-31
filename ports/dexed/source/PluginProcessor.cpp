f0706a8709fcf83d8512b1cd966b03f3055cc81a
falkTX <falktx@gmail.com>
2014-08-31 07:57:14 +0100

    Fix Dexed being silent, closes #4

--- ports/dexed/source/PluginProcessor.cpp
+++ ports/dexed/source/PluginProcessor.cpp
@@ -230,13 +230,6 @@ void DexedAudioProcessor::processBlock(AudioSampleBuffer& buffer, MidiBuffer& mi
     for (int channel = 1; channel < getNumInputChannels(); ++channel) {
         buffer.copyFrom(channel, 0, channelData, numSamples, 1);
     }
-
-    // In case we have more outputs than inputs, we'll clear any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    for (int i = getNumInputChannels(); i < getNumOutputChannels(); ++i) {
-        buffer.clear (i, 0, buffer.getNumSamples());
-    }
 }
 
 
