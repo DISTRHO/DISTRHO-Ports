2cce24c94e05128ac4cd9cf3f66fbe6bd7ff50ad
falkTX <falktx@gmail.com>
2015-03-05 17:26:42 +0100

    Build juce crypto and marketplace

--- libs/juce/build-juce/premake.lua
+++ libs/juce/build-juce/premake.lua
@@ -17,10 +17,12 @@ package.files = {
     "../source/modules/juce_audio_processors/juce_audio_processors.cpp",
     "../source/modules/juce_audio_utils/juce_audio_utils.cpp",
     "../source/modules/juce_core/juce_core.cpp",
+    "../source/modules/juce_cryptography/juce_cryptography.cpp",
     "../source/modules/juce_data_structures/juce_data_structures.cpp",
     "../source/modules/juce_events/juce_events.cpp",
     "../source/modules/juce_graphics/juce_graphics.cpp",
     "../source/modules/juce_gui_basics/juce_gui_basics.cpp",
-    "../source/modules/juce_gui_extra/juce_gui_extra.cpp"
+    "../source/modules/juce_gui_extra/juce_gui_extra.cpp",
+    "../source/modules/juce_tracktion_marketplace/juce_tracktion_marketplace.cpp"
   )
 }
