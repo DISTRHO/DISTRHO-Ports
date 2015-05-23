a6ef2e4f32e0af594351e8483021210e31a1a7a9
falkTX <falktx@gmail.com>
2015-05-23 18:39:13 +0200

    Fix some issues with juce vst host windows

--- libs/juce/source/modules/juce_audio_processors/format_types/juce_VSTPluginFormat.cpp
+++ libs/juce/source/modules/juce_audio_processors/format_types/juce_VSTPluginFormat.cpp
@@ -1284,7 +1284,12 @@ public:
 
             case audioMasterSizeWindow:
                 if (AudioProcessorEditor* ed = getActiveEditor())
+                {
+               #if JUCE_LINUX
+                    const MessageManagerLock mmLock;
+               #endif
                     ed->setSize (index, (int) value);
+                }
 
                 return 1;
 
@@ -1973,9 +1978,9 @@ public:
            #elif JUCE_LINUX
             if (pluginWindow != 0)
             {
-                XResizeWindow (display, pluginWindow, getWidth(), getHeight());
-                XMoveWindow (display, pluginWindow, pos.getX(), pos.getY());
+                XMoveResizeWindow (display, pluginWindow, pos.getX(), pos.getY(), getWidth(), getHeight());
                 XMapRaised (display, pluginWindow);
+                XFlush (display);
             }
            #endif
 
