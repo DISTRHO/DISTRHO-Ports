986afb100e3bfccd3ea3cc3d7188b2e6bb48d987
falkTX <falktx@falktx.com>
2020-01-26 17:26:19 +0000

    Fix build against gcc9 (compiler bug) Closes #45
    Signed-off-by: falkTX <falktx@falktx.com>

--- ports/easySSP/LV2/premake.lua
+++ ports/easySSP/LV2/premake.lua
@@ -3,6 +3,9 @@ dofile("../../../scripts/make-project.lua")
 
 package = make_juce_lv2_project("EasySSP")
 
+-- FIX GCC9 compiler bug, see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=90006
+package.config["Release"].buildoptions = { "-fno-tree-slp-vectorize" }
+
 package.files = {
   matchfiles (
     "../source/*.cpp",
