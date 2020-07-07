deb1c5507a369bae3b01b9a06b80f835ce361df0
falkTX <falktx@falktx.com>
2020-07-07 18:19:02 +0100

    CI: Run lv2lint on the linux builds Closes #41
    Signed-off-by: falkTX <falktx@falktx.com>

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,7 +6,11 @@ set -e
 sudo apt-get install -y build-essential meson
 
 if [ "${TARGET}" = "linux" ]; then
-    sudo apt-get install -y pkg-config libfreetype6-dev libx11-dev libxcomposite-dev libxrender-dev libxcursor-dev libasound2-dev libgl1-mesa-dev libglu1-mesa-dev
+    sudo apt-get install -y \
+        pkg-config \
+        libasound2-dev libfreetype6-dev libgl1-mesa-dev libglu1-mesa-dev \
+        libx11-dev libxcomposite-dev libxrender-dev libxcursor-dev \
+        lilv-utils lv2-dev lv2lint kxstudio-lv2-extensions
 
 elif [ "${TARGET}" = "win32" ]; then
     sudo apt-get install -y mingw-w64 binutils-mingw-w64-i686 g++-mingw-w64-i686 g++-multilib wine-stable wine32
--- .travis/script.sh
+++ .travis/script.sh
@@ -11,6 +11,11 @@ elif [ "${TARGET}" = "win64" ]; then
 fi
 
 echo "meson build --buildtype release ${EXTRA_ARGS}"
-meson build --buildtype release ${EXTRA_ARGS}
+meson build --buildtype=release --prefix=/usr ${EXTRA_ARGS}
 ninja -v -C build
 ninja -C build install
+
+if [ "${TARGET}" = "linux" ]; then
+    export LV2_PATH=${DESTDIR}/usr/lib/lv2:/usr/lib/lv2
+    for uri in $(lv2ls); do lv2lint -W lv2_generate_ttl -Snowarn ${uri}; done
+fi
