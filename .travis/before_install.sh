612078156ea572c5791a29c4553df672dc777a4f
falkTX <falktx@falktx.com>
2020-07-07 11:59:03 +0100

    CI: enable i386 multiarch for win32 cross-build
    Signed-off-by: falkTX <falktx@falktx.com>

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,6 +5,10 @@ set -e
 sudo add-apt-repository -y ppa:kxstudio-debian/kxstudio
 sudo add-apt-repository -y ppa:kxstudio-debian/toolchain
 
+if [ "${TARGET}" = "win32" ]; then
+    sudo dpkg --add-architecture i386
+fi
+
 sudo apt-get update -qq
 sudo apt-get install kxstudio-repos
 sudo apt-get update -qq
